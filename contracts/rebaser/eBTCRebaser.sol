e166ed5c3b82d5c588b780693ef7bf51ea08928a
Garfield Lee <openpgp@550.moe>
2021-05-26 15:20:34 +0800

    fix: code hash

--- contracts/rebaser/eBTCRebaser.sol
+++ contracts/rebaser/eBTCRebaser.sol
@@ -854,7 +854,7 @@ contract eBTCRebaser {
                         hex"ff",
                         factory,
                         keccak256(abi.encodePacked(token0, token1)),
-                        hex"96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f" // init code hash
+                        hex"9b025805045f6da539a4926f67307a9b0ab5aaccc42d3f14aaea0776a5b727ac" // init code hash
                     )
                 )
             )
